efe8714732ff5c0060383dc1239518a3c4b1bae5
FATY <laminefaty5261@gmail.com>
2023-09-22 11:48:52 +0000

    mise en place de l'architecture et des entites

---
+++ src/main/java/sn/faty/Formation_Spring/FormationSpringApplication.java
@@ -0,0 +1,13 @@
+package sn.faty.Formation_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationSpringApplication.class, args);
+	}
+
+}
